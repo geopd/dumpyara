5a76043f768ddad7d3b989b4591a411e72c86328
GeoPD <geoemmanuelpd2001@gmail.com>
2021-05-24 18:34:49 +0530

    dumpyara: Install Multiverse repository packages explicitly

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ "$(command -v apt)" != "" ]]; then
-        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
+        sudo apt install unace zip unzip p7zip-full sharutils uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename
+        wget http://archive.ubuntu.com/ubuntu/pool/multiverse/{p/p7zip-rar/p7zip-rar_16.02-2_amd64.deb,r/rar/rar_5.5.0-1_amd64.deb,u/unrar-nonfree/unrar_5.5.8-1_amd64.deb}
+        sudo dpkg -i p7zip* rar* unrar*
+        rm -rf p7zip* rar* unrar*
     elif [[ "$(command -v pacman)" != "" ]]; then
         sudo pacman -Sy --noconfirm unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc python-pip brotli axel gawk aria2 detox cpio
     fi
